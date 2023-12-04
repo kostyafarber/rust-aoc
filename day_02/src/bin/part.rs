5d20752af1247ef18c8478ea7998c81bbff3d2a4
Kostya Farber <kostya.farber@gmail.com>
2023-12-04 20:39:05 +0000

    add solution in words for day 2 part 2

---
+++ day_02/src/bin/part.rs
@@ -0,0 +1,10 @@
+/*
+The minimum number of cubes to make a game possible will be:
+    for each line:
+        record the max number for each colour seen.
+
+        for each set of max numbers for each colour on each line:
+            multiply them by each other
+            add them add to a cum sum
+*/
+fn main() {}
