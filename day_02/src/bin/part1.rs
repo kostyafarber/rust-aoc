36a20cb6ca2944c8316f41a930e7fffc74a9e297
Kostya Farber <kostya.farber@gmail.com>
2023-12-04 20:26:00 +0000

    finish day two part one

--- day_02/src/bin/part1.rs
+++ day_02/src/bin/part1.rs
@@ -3,33 +3,68 @@ use std::collections::HashMap;
 /*
 The Elf would first like to know which games would have been possible if the bag contained only
 12 red cubes, 13 green cubes, and 14 blue cubes?
+
+for each line:
+    * split into draws
+    * for each draw split into pairs of (number, colour)
+
+    for each pair:
+        check if number is greater than the number for that colour in the hashmap:
+            if so:
+                add game id to sum of game ids
 */
 
-fn main() {
-    let cubes = HashMap::from([("red", 12), ("green", 13), ("blue", 14)]);
-    let input = include_str!("input.txt");
+fn get_game_id(line: &str) -> i32 {
+    let idx_colon = line.find(":").unwrap();
+    let idx_space = line.find(" ").unwrap();
 
-    let lines: Vec<&str> = input.lines().collect();
+    let id = &line[idx_space + 1..idx_colon];
+    let id = id.parse::<i32>().unwrap();
 
-    let line = lines[0];
-    let idx = line.find(":").unwrap();
-    let line = &line[idx + 2..];
+    return id;
+}
 
-    let draws: Vec<&str> = line.split("; ").collect();
-    let draw: Vec<&str> = draws[0].split(", ").collect();
-    let pair: Vec<&str> = draw[0].split_whitespace().collect();
+fn is_impossible(draw: &str) -> bool {
+    let cubes: HashMap<&str, i32> = HashMap::from([("red", 12), ("green", 13), ("blue", 14)]);
 
-    println!("{:?}", line);
-    println!("{:?}", draws);
-    println!("{:?}", draw);
-    println!("{:?}", pair);
+    let pair: Vec<&str> = draw.split_whitespace().collect();
 
     let digit = pair[0].parse::<i32>().unwrap();
     let colour = pair[1];
-
     let max_colour = cubes.get(colour).unwrap();
 
-    println!("digit is: {:?}", digit);
-    println!("colour is: {colour}",);
-    println!("max colour is: {:?}", max_colour);
+    if digit > *max_colour {
+        return true;
+    }
+    return false;
+}
+
+fn process_line(line: &str) -> i32 {
+    let id = get_game_id(line);
+    let idx_colon = line.find(":").unwrap();
+    let line = &line[idx_colon + 2..];
+    let draws: Vec<&str> = line.split("; ").collect();
+
+    for draw in draws {
+        let pairs: Vec<&str> = draw.split(", ").collect();
+
+        for pair in pairs {
+            if is_impossible(pair) {
+                return 0;
+            }
+        }
+    }
+    return id;
+}
+
+fn main() {
+    let input = include_str!("input.txt");
+    let mut total_game_sum = 0;
+
+    let lines: Vec<&str> = input.lines().collect();
+
+    for line in lines {
+        total_game_sum += process_line(line)
+    }
+    println!("{total_game_sum}");
 }
