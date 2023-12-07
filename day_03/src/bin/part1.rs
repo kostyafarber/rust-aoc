3d22b729ef2cff6b884b45db5efed2b9e141c4f0
Kostya Farber <kostya.farber@gmail.com>
2023-12-07 18:47:10 +0000

    complete day 3 part 1

--- day_03/src/bin/part1.rs
+++ day_03/src/bin/part1.rs
@@ -13,7 +13,57 @@ iterate over all the chars:
 
 */
 
-fn check_adjacent() {}
+struct Bounds {
+    left: i32,
+    right: i32,
+    up: i32,
+    down: i32,
+}
+struct Puzzle {
+    grid: Vec<Vec<char>>,
+}
+
+impl Puzzle {
+    pub fn in_bounds(&self, x: i32, y: i32) -> bool {
+        let bounds = Bounds {
+            left: 0,
+            right: (self.grid[0].len()) as i32,
+            up: 0,
+            down: (self.grid.len()) as i32,
+        };
+        if x <= bounds.left || x >= bounds.right || y < bounds.up || y >= bounds.down {
+            // println!("{x}, {y} not in bounds");
+            return false;
+        }
+        return true;
+    }
+
+    pub fn check_adjacent(&self, x: i32, y: i32) -> bool {
+        let pairs: Vec<(i32, i32)> = vec![
+            (1, 0),
+            (0, 1),
+            (-1, 0),
+            (0, -1),
+            (-1, 1),
+            (1, -1),
+            (-1, -1),
+            (1, 1),
+        ];
+
+        for (dx, dy) in pairs {
+            let new_x = x + dx;
+            let new_y = y + dy;
+
+            if self.in_bounds(new_x, new_y) {
+                let pos = self.grid[new_x as usize][new_y as usize];
+                if !pos.is_digit(10) && pos != '.' {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
 
 fn make_grid(input: &str) -> Vec<Vec<char>> {
     let lines: Vec<Vec<char>> = input
@@ -26,7 +76,44 @@ fn make_grid(input: &str) -> Vec<Vec<char>> {
 }
 fn main() {
     let input = include_str!("input.txt");
-    let grid = make_grid(input);
 
-    for line in grid {}
+    let puzzle = Puzzle {
+        grid: make_grid(input),
+    };
+
+    let mut cum_sum = 0;
+    let mut number = 0;
+    let mut add = false;
+    let mut in_digit = false;
+
+    for (i, line) in puzzle.grid.iter().enumerate() {
+        println!("line {i}");
+        println!();
+        for (j, c) in line.iter().enumerate() {
+            if c.is_digit(10) {
+                in_digit = true;
+                number = (number * 10) + c.to_digit(10).unwrap();
+                if puzzle.check_adjacent(i as i32, j as i32) {
+                    add = true;
+                }
+            }
+
+            if !c.is_digit(10) || j == puzzle.grid.len() - 1 {
+                if add {
+                    print!("adding {number} to {cum_sum}, ");
+                    cum_sum += number;
+                    print!("sum is {cum_sum}\n");
+                    add = false;
+                }
+                if in_digit {
+                    in_digit = false;
+                    number = 0;
+                }
+            }
+        }
+        number = 0;
+        println!();
+        println!();
+    }
+    println!("Sum of parts are: {cum_sum}");
 }
